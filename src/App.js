64468bf100b55584a88bda758dfd2ecec933b73c
Chanuka Gunadasa <chanukag@zone24x7.com>
2020-08-14 15:27:32 +0530

    change layout

--- src/App.js
+++ src/App.js
@@ -16,21 +16,32 @@ function App() {
 
     useEffect(() => {
         initFaceApiAndUserMedia('videoPlaceholder');
+        // initFaceApiAndUserMedia('videoPlaceholder2');
     })
     return (
         <div className="App">
             <div className="container-wrapper">
                 <div className="container">
-                        {version}
+                    <h3>Lets make sure it's you</h3>
+                    <p>
+                        Before you placing your order, you must verify payment to Online Store for $275.99
+                    </p>
+                    <p>
+                        Please verify using face authentication
+                    </p>
 
-                        {/*{initFaceApiAndUserMedia('videoPlaceholder2')}*/}
+                    <button onClick={handleVideo}>AUTHENTICATE WITH FACE</button>
+                    <p>
+                        You are authorizing My Bank to get your facial data. Click <a href="#">here</a> to understand how we use your data.. View our privacy notice.
+                    </p>
 
-                    <button onClick={handleVideo}>Start Video - myCanvasWrapper</button>
+                    <strong>Having Trouble?</strong>
+                    <small><a href="#">Choose another security option</a></small>
+                    {/*<button onClick={handleVideo2}>Start Video - myCanvasWrapper 2</button>*/}
+                    {/*<div id="videoPlaceholder2"></div>*/}
+                    <div id="videoPlaceholder"></div>
                     <button onClick={stop}>stop</button>
-                        {/*<button onClick={handleVideo2}>Start Video - myCanvasWrapper 2</button>*/}
-
-                        <div id="videoPlaceholder"></div>
-                        {/*<div id="videoPlaceholder2"></div>*/}
+                    {version}
                 </div>
             </div>
         </div>
