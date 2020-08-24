ce25c5dfa12f1961752a01d2a8c292970d1645da
samudithaw <samudithaw@zone24x7.com>
2020-08-24 09:14:24 +0530

    Updated Page Styles

--- src/App.js
+++ src/App.js
@@ -29,35 +29,39 @@ function App(props) {
         // initFaceApiAndUserMedia('videoPlaceholder2');
     })
     return (
-        <div className="App">
-            <div className="header">
-                <div className="logo logo1"></div>
-                <div className="logo logo2"></div>
+        <div className="faume-fe-wrapper">
+            <div className="faume-fe-header">
+                <div className="faume-fe-logo"></div>
+                <div className="faume-fe-visa-logo"></div>
             </div>
-            <div className="container-wrapper">
-                <div className="container">
-                    <div className="title">Lets make sure it's you</div>
-                    <p>
-                        Before you placing your order, you must verify payment to Online Store for $275.99
+            <div className="faume-fe-content-wrapper">
+                <div className="faume-fe-container">
+                    <div className="faume-fe-text-heading">Lets make sure it's you</div>
+                    <p className="faume-fe-text-content">
+                        Before placing your order,  you must verify payment to Online Store for $275.99
                     </p>
-                    <p>
-                        Please verify using face authentication
-                    </p>
+                    <ul>
+                        <li>Please verify using face authentication</li>
+                    </ul>
 
-                    <button className="btn fa-btn videoPlaceholder-btn" onClick={handleVideo}><span>AUTHENTICATE WITH FACE</span></button>
-                    <p>
+                    <button className="faume-fe-btn videoPlaceholder-btn" onClick={handleVideo}><span>AUTHENTICATE WITH FACE</span></button>
+                    <p className="faume-fe-text-content">
                         You are authorizing My Bank to get your facial data. Click <a href="#">here</a> to understand how we use your data.. View our privacy notice.
                     </p>
 
-                    <div className="support">Having Trouble?</div>
-                    <div className="security-option"><a href="#">Choose another security option</a></div>
+                    <div className="faume-fe-support">Having Trouble?</div>
+                    <a href="#" className="faume-fe-security-option">Choose another security option</a>
                     {/*<button onClick={handleVideo2}>Start Video - myCanvasWrapper 2</button>*/}
                     {/*<div id="videoPlaceholder2"></div>*/}
                     <div id="videoPlaceholder"></div>
-                    <button className="btn stop" onClick={stop}>stop</button>
-                    <small>{version}</small>
+                    <button className="faume-fe-btn stop" onClick={stop}>stop</button>
                 </div>
             </div>
+            <div className="faume-fe-content-footer">
+                <div className="faume-fe-footer-content-link">Need Help?</div>
+                <div className="faume-fe-footer-content-link-plus">+</div>
+            </div>
+            <small className="faume-fe-version">{version}</small>
         </div>
     );
 }
